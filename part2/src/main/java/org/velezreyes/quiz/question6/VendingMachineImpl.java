d7ca6cd2b936a2045fe42613bc219c557bea054e
jubuenor <jubuenor@unal.edu.co>
2023-11-04 15:54:34 -0500

    part 2 solution fixes

--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -5,12 +5,21 @@ import java.util.Hashtable;
 
 public class VendingMachineImpl implements VendingMachine {
   private double currentMoney;
-  private Dictionary<String, DrinkImpl> drinks = new Hashtable<String, DrinkImpl>();
+  private Dictionary<String, DrinkImpl> drinks;
 
   public VendingMachineImpl() {
     this.currentMoney = 0;
-    this.drinks.put("ScottCola", new DrinkImpl("ScottCola", 0.75, true));
-    this.drinks.put("KarenTea", new DrinkImpl("KarenTea", 1.0, false));
+    this.drinks = new Hashtable<String, DrinkImpl>();
+    this.addDrink(new DrinkImpl("ScottCola", 0.75, true));
+    this.addDrink(new DrinkImpl("KarenTea", 1.0, false));
+  }
+
+  public void addDrink(DrinkImpl drink) {
+    this.drinks.put(drink.getName(), drink);
+  }
+
+  public void removeDrink(String name) {
+    this.drinks.remove(name);
   }
 
   public double getCurrentMoney() {
